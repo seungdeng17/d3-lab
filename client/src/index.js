4c79c52c6dc0fddae5eeff0bafe460e5e13ef719
이승택 <seungdeng17@gmail.com>
2021-06-17 12:26:09 +0900

    line chart

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,141 @@
-import { $ } from "./utils";
+import * as d3 from "d3";
+import "./styles.css";
 
-const $sample01 = $(".sample01");
+// set the dimensions and margins of the graph
+const margin = { top: 10, right: 30, bottom: 30, left: 60 },
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+const dataURL =
+  "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv";
+
+let svg = d3
+  .select(".sample01")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+async function draw() {
+  let data = await d3.csv(dataURL);
+  data = data.map((d) => ({
+    date: d3.timeParse("%Y-%m-%d")(d.date),
+    value: d.value,
+  }));
+
+  let x = d3
+    .scaleTime()
+    .domain(
+      d3.extent(data, function (d) {
+        return d.date;
+      })
+    )
+    .range([0, width]);
+  let xAxis = svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+  // Add Y axis
+  var y = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max(data, function (d) {
+        return +d.value;
+      }),
+    ])
+    .range([height, 0]);
+  let yAxis = svg.append("g").call(d3.axisLeft(y));
+
+  // Add a clipPath: everything out of this area won't be drawn.
+  var clip = svg
+    .append("defs")
+    .append("svg:clipPath")
+    .attr("id", "clip")
+    .append("svg:rect")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("x", 0)
+    .attr("y", 0);
+
+  // Add brushing
+  var brush = d3
+    .brushX() // Add the brush feature using the d3.brush function
+    .extent([
+      [0, 0],
+      [width, height],
+    ]) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+    .on("end", updateChart); // Each time the brush selection changes, trigger the 'updateChart' function
+
+  // Create the line variable: where both the line and the brush take place
+  var line = svg.append("g").attr("clip-path", "url(#clip)");
+
+  // Add the line
+  line
+    .append("path")
+    .datum(data)
+    .attr("class", "line") // I add the class line to be able to modify this line later on.
+    .attr(
+      "d",
+      d3
+        .line()
+        .x(function (d) {
+          return x(d.date);
+        })
+        .y(function (d) {
+          return y(d.value);
+        })
+    );
+
+  // Add the brushing
+  line.append("g").attr("class", "brush").call(brush);
+
+  let idleTimeout;
+  function idled() {
+    idleTimeout = null;
+  }
+
+  function updateChart() {
+    let extent = d3.event.selection;
+    if (!extent) {
+      if (!idleTimeout) return (idleTimeout = setTimeout(idled, 350));
+      x.domain([4, 8]);
+    } else {
+      x.domain([x.invert(extent[0]), x.invert(extent[1])]);
+      line.select(".brush").call(brush.move, null);
+    }
+
+    xAxis.transition().duration(1000).call(d3.axisBottom(x));
+
+    line
+      .select(".line")
+      .transition()
+      .duration(1000)
+      .attr(
+        "d",
+        d3
+          .line()
+          .x((d) => x(d.date))
+          .y((d) => y(d.value))
+      );
+  }
+
+  svg.on("dblclick", function () {
+    x.domain(d3.extent(data, (d) => d.date));
+    xAxis.transition().call(d3.axisBottom(x));
+    line
+      .select(".line")
+      .transition()
+      .attr(
+        "d",
+        d3
+          .line()
+          .x((d) => x(d.date))
+          .y((d) => y(d.value))
+      );
+  });
+}
+
+draw();
